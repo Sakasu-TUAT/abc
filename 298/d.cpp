ab500e8d81cdbc5b2f92c3d0d8556ecbed93e6e3
Sota AKASAKA <s214229y@st.go.tuat.ac.jp>
2023-04-19 00:10:18 +0900

    ac-libraryの追加

--- 298/d.cpp
+++ 298/d.cpp
@@ -1,53 +1,29 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <iomanip>
-#include <limits>
-#include <deque>
-#include <set>
+#include <bits/stdc++.h>
+#include <atcoder/modint>
+
 using namespace std;
-#define Out(x) cout << x << endl
-#define Yes "Yes"
-#define No "No"
-#define Ans cout << ans << endl
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define rrep(i, a, b) for (int i = a; i >= b; i--)
-#define fore(i, a) for (auto &i : a)
-using ll = long long;
-using P = pair<int, int>;
-// const ll mod = 10e9+7;
+using namespace atcoder;
+using mint = modint998244353;
 
 
 int main(){
-	ll n, q;
-    cin >> n >> q;
-    vector<vector<int>> box(200010, vector<int>());
-    vector<set<int>> num(200010);
+	int q;
+    cin >> q;
+    deque<int> d(1, 1);
+    mint ans = 1;
     while(q--){
-        ll x; cin >> x;
-        if(x==1){
-            ll i, j;
-            cin >> i >> j;
-            box[j].emplace_back(i);
-            num[i].insert(j);
+        int i; cin >> i;
+        if(i==1){
+            int x; cin >> x;
+            d.push_back(x);
+            ans = 10*ans + x;
         }
-        if(x==2){
-            ll i; cin >> i;
-            sort(box[i].begin(), box[i].end());
-            for(const auto &v : box[i]){
-                cout << v << " ";
-            }
-            cout << endl;
+        if(i==2){
+            ans -= mint(10).pow(d.size()-1)*d.front();
+            d.pop_front();
         }
-        if(x==3){
-            ll i; cin >> i;
-            for(const auto &v : num[i]){
-                cout << v << " ";
-            }
-            cout << endl;
+        if(i==3){
+            cout << ans.val() << endl;
         }
     }
 }
