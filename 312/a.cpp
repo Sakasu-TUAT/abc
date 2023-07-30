fd595ed24d235c04786cd650168bbe24f2e21e13
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-30 12:15:57 +0900

    abc312

---
+++ 312/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string n; cin >> n;
+   if(n=="ACE" or n=="BDF" or n=="CEG" or n=="DFA" or n=="EGB" or n=="FAC" or n=="GBD"){
+    cout << "Yes" << endl;
+   } else {
+    cout << "No" << endl;
+   }
+    return 0;
+} 
